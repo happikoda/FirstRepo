eaa2ee7db868151a627030f2f0fa45591b83b855
happikoda <happikoda@gmail.com>
2022-06-12 17:52:33 +0530

    Add java file 2nd program

---
+++ Add.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Add{
+public static void main(String[] args){
+int a,b,c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the values for a and b ");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a+b;
+System.out.println(c);
+}
+}
